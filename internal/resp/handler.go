621243bbd35a1349d21096806e731db0e85e057b
perdomo <dropdibemol@gmail.com>
2026-05-31 02:17:58 -0500

    feat(resp): implement command dispatch and RESP handler

---
+++ internal/resp/handler.go
@@ -0,0 +1,61 @@
+package resp
+
+import (
+	"io"
+
+	"github.com/AlexisPerdomo/redix/internal/protocol"
+)
+
+func handleSimpleStr(s string, w io.Writer) error {
+	if s == "PING" {
+		return protocol.WrSimpleStr("PONG", w)
+	}
+
+	return protocol.WrErr("ERR unknown command", w)
+}
+
+func handleBulkStr(s string, w io.Writer) error {
+	return protocol.WrErr("ERR unexpected bulk string", w)
+}
+
+func handleErr(s string, w io.Writer) error {
+	return protocol.WrErr("ERR received error", w)
+}
+
+func handleInt(s int64, w io.Writer) error {
+	return protocol.WrErr("ERR clients cannot send integer type", w)
+}
+
+func handleArray(s []any, w io.Writer) error {
+	if len(s) == 0 {
+		return protocol.WrErr("ERR empty array", w)
+	}
+
+	cmdVal, ok := s[0].(*protocol.RESPVal)
+	if !ok {
+		return protocol.WrErr("ERR invalid command format", w)
+	}
+
+	cmd, ok := cmdVal.Val.(string)
+	if !ok {
+		return protocol.WrErr("ERR invalid command type value", w)
+	}
+
+	switch RESPCommand(cmd) {
+	case RESPCommandGet:
+		return protocol.WrErr("ERR not implemented", w)
+	case RESPCommandSet:
+		return protocol.WrErr("ERR not implemented", w)
+	case RESPCommandDel:
+		return protocol.WrErr("ERR not implemented", w)
+	case RESPCommandKeys:
+		return protocol.WrErr("ERR not implemented", w)
+	case RESPCommandExists:
+		return protocol.WrErr("ERR not implemented", w)
+	case RESPCommandPing:
+		return protocol.WrSimpleStr("PONG", w)
+	default:
+		return protocol.WrErr("ERR unknown command", w)
+
+	}
+}
--- internal/resp/resp.go
+++ internal/resp/resp.go
@@ -1,2 +1,70 @@
 // Package resp provides the compatibility layer for the redis protocol (RESP).
 package resp
+
+import (
+	"errors"
+	"io"
+
+	"github.com/AlexisPerdomo/redix/internal/protocol"
+)
+
+type RESPCommand string
+
+const (
+	RESPCommandGet    RESPCommand = "GET"
+	RESPCommandSet    RESPCommand = "SET"
+	RESPCommandDel    RESPCommand = "DEL"
+	RESPCommandKeys   RESPCommand = "KEYS"
+	RESPCommandExists RESPCommand = "EXISTS"
+	RESPCommandPing   RESPCommand = "PING"
+)
+
+var (
+	ErrUnknownType    = errors.New("unknown type")
+	ErrInvalidType    = errors.New("invalid type assertion")
+	ErrUnknownCommand = errors.New("unknown command")
+	ErrInvalidCommand = errors.New("invalid command")
+)
+
+func Handle(val *protocol.RESPVal, w io.Writer) error {
+	switch val.Type {
+	case protocol.RESPTypeSimpleString:
+		v, ok := val.Val.(string)
+		if !ok {
+			return ErrInvalidType
+		}
+
+		return handleSimpleStr(v, w)
+
+	case protocol.RESPTypeError:
+		v, ok := val.Val.(string)
+		if !ok {
+			return ErrInvalidType
+		}
+		return handleErr(v, w)
+
+	case protocol.RESPTypeInteger:
+		v, ok := val.Val.(int64)
+		if !ok {
+			return ErrInvalidType
+		}
+
+		return handleInt(v, w)
+	case protocol.RESPTypeBulkString:
+		v, ok := val.Val.(string)
+		if !ok {
+			return ErrInvalidType
+		}
+
+		return handleBulkStr(v, w)
+	case protocol.RESPTypeArray:
+		v, ok := val.Val.([]any)
+		if !ok {
+			return ErrInvalidType
+		}
+
+		return handleArray(v, w)
+	default:
+		return ErrUnknownType
+	}
+}
